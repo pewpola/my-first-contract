29c8cd0c7315c8a97d9e2f4aa4556f19c63f52ba
Eduardo <eduardojor77@gmail.com>
2024-03-28 14:48:00 -0300

    supporting two players now

--- contracts/EvenOrOdd.sol
+++ contracts/EvenOrOdd.sol
@@ -10,21 +10,35 @@ contract OddOrEven {
     uint8 private numberPlayer1;
     string public status = "";
 
-    function compare(string memory str1, string memory str2) private pure returns(bool) {
+    function compare(
+        string memory str1,
+        string memory str2
+    ) private pure returns (bool) {
         bytes memory arrA = bytes(str1);
         bytes memory arrB = bytes(str2);
-        return arrA.length == arrB.length && keccak256(arrA) == keccak256(arrB);           
+        return arrA.length == arrB.length && keccak256(arrA) == keccak256(arrB);
     }
 
     function choose(string memory newChoice) public {
-        require(compare(newChoice, "EVEN") || compare(newChoice, "ODD"), "Choose EVEN OR ODD");
+        require(
+            compare(newChoice, "EVEN") || compare(newChoice, "ODD"),
+            "Choose EVEN OR ODD"
+        );
 
-        string memory message = string.concat("Player 1 already choose ", choicePlayer1);
-        require(compare(choicePlayer1, ""));
+        string memory message = string.concat(
+            "Player 1 already choose ",
+            choicePlayer1
+        );
+        require(compare(choicePlayer1, ""), message);
 
         choicePlayer1 = newChoice;
         player1 = msg.sender;
-        status = string.concat("Player 1 is ", player1, " and choose ", choicePlayer1);
+        status = string.concat(
+            "Player 1 is ",
+            Strings.toHexString(player1),
+            " and choose ",
+            choicePlayer1
+        );
     }
 
     /*
@@ -34,25 +48,38 @@ contract OddOrEven {
     */
 
     function play(uint8 number) public {
-        require(!compare(choicePlayer1, ""), "Choose your option first. EVEN or ODD");
+        require(
+            !compare(choicePlayer1, ""),
+            "Choose your option first. EVEN or ODD"
+        );
+        require(number > 0, "The number must be greater than 0.");
 
-        if(msg.sender) {
+        if (msg.sender == player1) {
             numberPlayer1 = number;
-            status = "Player 1 already played. Waiting player 2.";
+            status = "Player 1 already played. Waiting Player 2.";
         } else {
-            uint256 cpuNumber = ramdom();
-            bool isEven = (number + cpuNumber) % 2 == 0;
-            string memory message = string.concat("Player choose ", choice , 
-            " and plays ", Strings.toString(number), 
-            ". CPU plays ", Strings.toString(cpuNumber));
+            require(numberPlayer1 != 0, "Player 1 needs to play first");
+            bool isEven = (numberPlayer1 + number) % 2 == 0;
+            string memory message = string.concat(
+                "Player 1 choose ",
+                choicePlayer1,
+                " and plays ",
+                Strings.toString(numberPlayer1),
+                ". Player 2 plays ",
+                Strings.toString(number)
+            );
 
-            if(isEven && compare(choicePlayer1, "EVEN")) {
-                return string.concat(message, ". Player won.");
-            } else if(!isEven && compare(choicePlayer1, "ODD")) {
-                return string.concat(message, ". Player won.");
+            if (isEven && compare(choicePlayer1, "EVEN")) {
+                status = string.concat(message, ". Player 1 won.");
+            } else if (!isEven && compare(choicePlayer1, "ODD")) {
+                status = string.concat(message, ". Player 1 won.");
             } else {
-                return string.concat(message, ". Player won.");
+                status = string.concat(message, ". Player 2 won.");
             }
+
+            choicePlayer1 = "";
+            numberPlayer1 = 0;
+            player1 = address(0);
         }
     }
 }
