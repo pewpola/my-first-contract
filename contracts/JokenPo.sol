91706ec733f53cce63edceed1b12c5fb078914de
Eduardo <eduardojor77@gmail.com>
2024-04-02 16:20:35 -0300

    enum options and variables initialized

--- contracts/JokenPo.sol
+++ contracts/JokenPo.sol
@@ -1,3 +1,17 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.12;
+
+contract JokenPo {
+    enum Options {
+        NONE,
+        ROCK,
+        PAPER,
+        SCISSORS
+    }
+
+    Options private choice1 = Options.NONE;
+    address private player1;
+    string public result = "";
+
+}
