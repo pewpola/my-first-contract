39bfdf1508f0a4cfddb98dbac1e8e5aa80ccd8e8
Eduardo <eduardojor77@gmail.com>
2024-04-03 15:52:52 -0300

    adapted code for function update

--- contracts/JokenPo.sol
+++ contracts/JokenPo.sol
@@ -3,6 +3,7 @@
 pragma solidity ^0.8.12;
 
 contract JokenPo {
+    
     enum Options {
         NONE,
         ROCK,
@@ -29,33 +30,19 @@ contract JokenPo {
             choice1 = newChoice;
             result = "Player 1 choose his/her option. Waiting player 2.";
         } else if (choice1 == Options.ROCK && newChoice == Options.SCISSORS) {
-            result = "Rock breaks scissors. Player 1 won.";
-            player1 = address(0);
-            choice1 = Options.NONE;
+            update("Rock breaks scissors. Player 1 won.");
         } else if (choice1 == Options.PAPER && newChoice == Options.ROCK) {
-            result = "Paper wraps rock. Player 1 won.";
-            player1 = address(0);
-            choice1 = Options.NONE;
+            update("Paper wraps rock. Player 1 won.");
         } else if (choice1 == Options.SCISSORS && newChoice == Options.PAPER) {
-            result = "Scissores cuts paper. Player 1 won.";
-            player1 = address(0);
-            choice1 = Options.NONE;
+            update("Scissores cuts paper. Player 1 won.");
         } else if (choice1 == Options.SCISSORS && newChoice == Options.ROCK) {
-            result = "Rock breaks scissors. Player 2 won.";
-            player1 = address(0);
-            choice1 = Options.NONE;
+            update("Rock breaks scissors. Player 2 won.");
         } else if (choice1 == Options.ROCK && newChoice == Options.PAPER) {
-            result = "Paper wraps rock. Player 2 won.";
-            player1 = address(0);
-            choice1 = Options.NONE;
-        } else if (choice1 == Options.PAPER && newChoice == Options.SCISSORS) {
-            result = "Scissores cuts paper. Player 2 won.";
-            player1 = address(0);
-            choice1 = Options.NONE;
+            update("Paper wraps rock. Player 2 won.");
+       } else if (choice1 == Options.PAPER && newChoice == Options.SCISSORS) {
+            update("Scissores cuts paper. Player 2 won.");
         } else {
-            result = "Draw game.";
-            player1 = address(0);
-            choice1 = Options.NONE;
+            update("Draw game.");
         }
     }
 }
