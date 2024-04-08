478df21f7fa6fc54468f65e0c1536969e310dedd
Eduardo <eduardojor77@gmail.com>
2024-04-08 14:53:07 -0300

    addBook function

--- contracts/BookDataBase.sol
+++ contracts/BookDataBase.sol
@@ -9,5 +9,11 @@ contract BookDataBase {
         uint16 year;
     }
 
+    uint32 private nextId = 0;
     mapping (uint32 => Book) public books;
+
+    function addBook(Book memory newBook) public {
+        nextId++;
+        books[nextId] = newBook;
+    } 
 }
